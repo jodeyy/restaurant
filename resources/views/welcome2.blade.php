64a04e72df688f86a1ffa6a9d3891d4b6bd63c77
jodey <jodiptra@gmail.com>
2024-07-05 01:56:48 +0700

    update

--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -1,114 +1,210 @@
+
 <!DOCTYPE html>
-<html>
-   <head>
-      <!-- basic -->
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <!-- mobile metas -->
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
-      <!-- site metas -->
-      <title>Onion Cafe</title>
-      <meta name="keywords" content="">
-      <meta name="description" content="">
-      <meta name="author" content="">
-      <!-- bootstrap css -->
-      <link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap.min.css') }}">
-      <!-- style css -->
-      <link rel="stylesheet" type="text/css" href="{{ url('css/style.css') }}">
-      <!-- Responsive-->
-      <link rel="stylesheet" href="{{ url('css/responsive.css') }}">
-      <!-- fevicon -->
-      <link rel="icon" href="{{ url('images/fevicon.png') }}" type="image/gif" />
-      <!-- font css -->
-      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;800&display=swap" rel="stylesheet">
-      <!-- Scrollbar Custom CSS -->
-      <link rel="stylesheet" href="{{ url('css/jquery.mCustomScrollbar.min.css') }}">
-      <!-- Tweaks for older IEs-->
-      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
-   </head>
-   <body>
-      <div class="header_section">
-         <div class="container">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-               <a class="navbar-brand"href="#"><img src="{{ url('images/logo2.png') }}" width="125px"></a>
-               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-               <span class="navbar-toggler-icon"></span>
-               </button>
-               <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                  <ul class="navbar-nav ml-auto">
-                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home</a>
-                     </li>
-                     <li class="nav-item">
-                        <a class="nav-link" href="https://www.instagram.com/onioncaferesto?igsh=djNubXoyYmluOGNs">Hubungi Kami</a>
-                     </li>
-                  </ul>
-                  <form class="form-inline my-2 my-lg-0">
-                     <div class="login_bt">
-                        <ul>
-                           <li><a href="{{ route('login') }}"><span class="user_icon"><i class="fa fa-user" aria-hidden="true"></i></span>Login</a></li>
-                        </ul>
-                     </div>
-                  </form>
-               </div>
-            </nav>
-         </div>
-         <!-- banner section start --> 
-         <div class="banner_section layout_padding">
-            <div class="container">
-               <div id="banner_slider" class="carousel slide" data-ride="carousel">
-                  <div class="carousel-inner">
-                     <div class="carousel-item active">
-                        <div class="row">
-                           <div class="col-md-12">
-                              <div class="banner_taital_main">
-                                 <h1 class="banner_taital">Onion <br>Cafe and Resto</h1>
-                                 <p class="banner_text">Jl. Rajawali No.440, 9 Ilir, Kec. Ilir Tim. II, Kota Palembang, Sumatera Selatan 30114</p>
-                                 <div class="btn_main">
-                                    <div class="about_bt active"><a href="#">Tentang Kami</a></div>
-                                    <div class="callnow_bt"><a href="https://www.instagram.com/onioncaferesto?igsh=djNubXoyYmluOGNs">Hubungi Kami</a></div>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                     <div class="carousel-item">
-                        <div class="row">
-                           <div class="col-md-12">
-                              <div class="banner_taital_main">
-                                 <h1 class="banner_taital">Onion<br>Cafe and Resto</h1>
-                                 <p class="banner_text">Jl. Rajawali No.440, 9 Ilir, Kec. Ilir Tim. II, Kota Palembang, Sumatera Selatan 30114</p>
-                                 <div class="btn_main">
-                                    <div class="about_bt active"><a href="#">Tentang Kami</a></div>
-                                    <div class="callnow_bt"><a href="https://www.instagram.com/onioncaferesto?igsh=djNubXoyYmluOGNs">Hubungi kami</a></div>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-                  <a class="carousel-control-prev" href="#banner_slider" role="button" data-slide="prev">
-                  <i class="fa fa-arrow-left"></i>
-                  </a>
-                  <a class="carousel-control-next" href="#banner_slider" role="button" data-slide="next">
-                  <i class="fa fa-arrow-right"></i>
-                  </a>
-               </div>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Restoran - Bootstrap Restaurant Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&family=Pacifico&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container-xxl bg-white p-0">
+        <!-- Spinner Start -->
+        <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+                <span class="sr-only">Loading...</span>
             </div>
-         </div>
-         <!-- banner section end -->
-      </div>
-      <!-- header section end -->
-      <!-- Javascript files-->
-      <script src="{{ url('js/jquery.min.js') }}"></script>
-      <script src="{{ url('js/popper.min.js') }}"></script>
-      <script src="{{ url('js/bootstrap.bundle.min.js') }}"></script>
-      <script src="{{ url('js/jquery-3.0.0.min.js') }}"></script>
-      <script src="{{ url('js/plugin.js') }}"></script>
-      <!-- sidebar -->
-      <script src="{{ url('js/jquery.mCustomScrollbar.concat.min.js') }}"></script>
-      <script src="{{ url('js/custom.js') }}"></script>
-   </body>
+        </div>
+        <!-- Spinner End -->
+
+
+        <!-- Navbar & Hero Start -->
+        <div class="container-xxl position-relative p-0">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-4 px-lg-5 py-3 py-lg-0">
+                <a href="" class="navbar-brand p-0">
+                    <h1 class="text-primary m-0"><i class="fa fa-utensils me-3"></i>Restoran</h1>
+                    <!-- <img src="img/logo.png" alt="Logo"> -->
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="fa fa-bars"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav ms-auto py-0 pe-4">
+                        <a href="#" class="nav-item nav-link active">Home</a>
+                        <a href="{{ route('register') }}" class="nav-item nav-link">Register</a>
+                        <a href="{{ route('login') }}" class="nav-item nav-link">Login</a>
+                        <a href="https://wa.me/6285268483791" class="nav-item nav-link">Contact</a>
+                    </div>
+                    <a href="{{ route('login') }}" class="btn btn-primary py-2 px-4">Book A Table</a>
+                </div>
+            </nav>
+
+            <div class="container-xxl py-5 bg-dark hero-header mb-5">
+                <div class="container my-5 py-5">
+                    <div class="row align-items-center g-5">
+                        <div class="col-lg-6 text-center text-lg-start">
+                            <h1 class="display-3 text-white animated slideInLeft">Welcome to<br>Volta Restaurant</h1>
+                            <p class="text-white animated slideInLeft mb-4 pb-2">Kami adalah tempat makan yang menawarkan pengalaman kuliner terbaik dengan menu yang beragam dan cita rasa yang autentik. Terletak di tengah kota, Restoran Volta menjadi destinasi favorit bagi para pecinta kuliner yang mencari suasana nyaman dan hidangan lezat.</p>
+                            <a href="https://wa.me/6285268483791" class="btn btn-primary py-sm-3 px-sm-5 me-3 animated slideInLeft">Book A Table</a>
+                        </div>
+                        <div class="col-lg-6 text-center text-lg-end overflow-hidden">
+                            <img class="img-fluid" src="img/hero.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Navbar & Hero End -->
+
+
+        <!-- Service Start -->
+        <div class="container-xxl py-5">
+            <div class="container">
+                <div class="row g-4">
+                    <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
+                        <div class="service-item rounded pt-3">
+                            <div class="p-4">
+                                <i class="fa fa-3x fa-user-tie text-primary mb-4"></i>
+                                <h5>Master Chefs</h5>
+                                <p>Dibuat dengan 10 Chef Handal</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
+                        <div class="service-item rounded pt-3">
+                            <div class="p-4">
+                                <i class="fa fa-3x fa-utensils text-primary mb-4"></i>
+                                <h5>Quality Food</h5>
+                                <p>Kualitas Makanan Bintang 5</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
+                        <div class="service-item rounded pt-3">
+                            <div class="p-4">
+                                <i class="fa fa-3x fa-cart-plus text-primary mb-4"></i>
+                                <h5>Online Reservation</h5>
+                                <p>Reservasi Online dengan mudah</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
+                        <div class="service-item rounded pt-3">
+                            <div class="p-4">
+                                <i class="fa fa-3x fa-headset text-primary mb-4"></i>
+                                <h5>24/7 Service</h5>
+                                <p>Keluhan dilayani 24/7</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Service End -->
+
+
+        <!-- About Start -->
+        <div class="container-xxl py-5">
+            <div class="container">
+                <div class="row g-5 align-items-center">
+                    <div class="col-lg-6">
+                        <div class="row g-3">
+                            <div class="col-6 text-start">
+                                <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.1s" src="img/about-1.jpg">
+                            </div>
+                            <div class="col-6 text-start">
+                                <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.3s" src="img/about-2.jpg" style="margin-top: 25%;">
+                            </div>
+                            <div class="col-6 text-end">
+                                <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.5s" src="img/about-3.jpg">
+                            </div>
+                            <div class="col-6 text-end">
+                                <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.7s" src="img/about-4.jpg">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <h5 class="section-title ff-secondary text-start text-primary fw-normal">About Us</h5>
+                        <h1 class="mb-4">Welcome to <i class="fa fa-utensils text-primary me-2"></i>Volta Restoran</h1>
+                        <p class="mb-4">Di Restoran Volta, kami menghadirkan pengalaman kuliner yang tak terlupakan dengan suasana yang hangat dan nyaman. Nikmati hidangan lezat yang disiapkan oleh para koki kami yang berpengalaman dan berbakat.
+                            Dengan lebih dari 15 tahun pengalaman di industri kuliner, kami telah menjadi tujuan favorit bagi pecinta makanan. Restoran kami menawarkan berbagai hidangan yang memadukan cita rasa tradisional dengan sentuhan modern, disajikan dengan keahlian dan perhatian terhadap detail.</p>
+                        <p class="mb-4">Kami bangga memiliki lebih dari 50 koki master yang terkenal, siap menyajikan hidangan terbaik untuk Anda. Dari makanan pembuka hingga hidangan penutup, setiap gigitan di Restoran Volta adalah sebuah perjalanan rasa yang mengesankan.
+                            Datang dan rasakan sendiri kelezatan menu kami. Kami siap menyambut Anda dengan pelayanan terbaik dan suasana yang ramah.</p>
+                        <div class="row g-4 mb-4">
+                            <div class="col-sm-6">
+                                <div class="d-flex align-items-center border-start border-5 border-primary px-3">
+                                    <h1 class="flex-shrink-0 display-5 text-primary mb-0" data-toggle="counter-up">15</h1>
+                                    <div class="ps-4">
+                                        <p class="mb-0">Years of</p>
+                                        <h6 class="text-uppercase mb-0">Experience</h6>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="d-flex align-items-center border-start border-5 border-primary px-3">
+                                    <h1 class="flex-shrink-0 display-5 text-primary mb-0" data-toggle="counter-up">50</h1>
+                                    <div class="ps-4">
+                                        <p class="mb-0">Popular</p>
+                                        <h6 class="text-uppercase mb-0">Master Chefs</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <a class="btn btn-primary py-3 px-5 mt-2" href="">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- About End -->
+
+
+       
+
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+    </div>
+
+    <!-- JavaScript Libraries -->
+    <script src={{ url("https://code.jquery.com/jquery-3.4.1.min.js")}}></script>
+    <script src={{ url("https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js")}}></script>
+    <script src={{ url("lib/wow/wow.min.js")}}></script>
+    <script src={{ url("lib/easing/easing.min.js")}}></script>
+    <script src={{ url("lib/waypoints/waypoints.min.js")}}></script>
+    <script src={{ url("lib/counterup/counterup.min.js")}}></script>
+    <script src={{ url("lib/owlcarousel/owl.carousel.min.js")}}></script>
+    <script src={{ url("lib/tempusdominus/js/moment.min.js")}}></script>
+    <script src={{ url("lib/tempusdominus/js/moment-timezone.min.js")}}></script>
+    <script src={{ url("lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js")}}></script>
+
+    <!-- Template Javascript -->
+    <script src={{ url("js/main.js")}}></script>
+</body>
+
 </html>
